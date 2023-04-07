a6938d67828eaa614e985b29654c4826fcb0d5a5
sgonza20 <sgonza20@vols.utk.edu>
2023-04-07 14:03:19 -0400

    Shared Config

--- providers.tf
+++ providers.tf
@@ -10,5 +10,6 @@ terraform {
 provider "aws" {
   # Configuration options
   region  = "us-east-1"
-  profile = "awscliprofile"
+  profile = "default"
+  shared_credentials_files = "/home/ec2-user/.aws/credentials"
 }
