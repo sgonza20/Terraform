3a80c668b8c62e5cf5b63ea628716239333565d7
Samuel Lozano <60197375+sgonza20@users.noreply.github.com>
2023-04-07 20:18:57 -0400

    updating kms.tf

--- modules/cm-kms/kms.tf
+++ modules/cm-kms/kms.tf
@@ -19,32 +19,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_kms" {
     }
   }
 }
-
-resource "aws_kms_key_policy" "kms_policy" {
-  key_id = var.kms_bucket_arn
-  policy = jsonencode({
-    Id = "example"
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "Enable KMS and CloudFront"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action = [
-            "kms:Decrypt",
-            "kms:Encrypt",
-            "kms:GenerateDataKey"
-        ]
-        Resource = "*"
-      }
-    ]
-    Condition = {
-        "StringEquals" = {
-            "AWS:": "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${var.cloudfront_distribution}"
-        }
-    }
-})
-}
